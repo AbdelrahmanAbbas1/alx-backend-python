139498d771ca4eab91dd01aa65c9cd3338b79f05
AbdelrahmanAbbas1 <abdelrahmanabbas599@gmail.com>
2024-11-09 14:03:58 +0200

    Adding new file

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""This module defines a functions with type annotations"""
+
+
+def to_str(n: float) -> str:
+    """Takes a float and returns a string"""
+    return str(n)
