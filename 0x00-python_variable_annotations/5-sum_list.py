836848a847e30ef5f2b3ac0b32e8ba85bf0799bd
AbdelrahmanAbbas1 <abdelrahmanabbas599@gmail.com>
2024-11-09 19:24:50 +0200

    Adding new file

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 """This module defines function with ytpe annotation"""
+from typing import List
 
 
-def sum_list(input_list: list[float]) -> float:
+def sum_list(input_list: List[float]) -> float:
     sum: float = 0
     for i in input_list:
         sum = sum + i
