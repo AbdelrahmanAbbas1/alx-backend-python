892ebfe3322978fc5c8729945dda2da8b92f465a
AbdelrahmanAbbas1 <abdelrahmanabbas599@gmail.com>
2024-11-18 13:40:36 +0200

    Adding new file

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -2,10 +2,10 @@
 """This module defines an async function"""
 import asyncio
 import random
-from typing import AsyncGenerator
+from typing import Generator
 
 
-async def async_generator() -> AsyncGenerator[float, None]:
+async def async_generator() -> Generator[float, None, None]:
     """Yields a random number every 1 second"""
     for i in range(10):
         await asyncio.sleep(1)
