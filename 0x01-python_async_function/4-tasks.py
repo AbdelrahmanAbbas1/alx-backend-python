2bf0dd931ded80dc76ca2f9ff370805fc7c9ed7b
AbdelrahmanAbbas1 <abdelrahmanabbas599@gmail.com>
2024-11-18 02:29:02 +0200

    Adding new file

---
+++ 0x01-python_async_function/4-tasks.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""This module defines asynchronous function"""
+import asyncio
+from typing import List
+
+task_wait_random = __import__('3-tasks').wait_random
+
+
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
+    """Returns a list of floats"""
+    tasks = [task_wait_random(max_delay) for i in range(n)]
+    wait_times = await asyncio.gather(*tasks)
+    return sorted(wait_times)
